0564d42967084f967c2a2ffbd56bc9954d2b430c
MHoussam <mohammadosta1999@gmail.com>
2023-08-09 09:23:16 +0300

    Client <> Created the Login & Registration Pages.

---
+++ instagram-client/src/Pages/Register/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+const Register = () => {
+  return (
+    <div>
+        <Register user={user} />
+    </div>
+  )
+}
+
+export default Register;
