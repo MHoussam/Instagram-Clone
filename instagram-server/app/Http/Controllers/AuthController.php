d7215dba7ea82bd2cbaffad3f7863abb7169e37e
MHoussam <mohammadosta1999@gmail.com>
2023-08-09 07:33:54 +0300

    Server <> Added the USername Attribute to the Authentication.

--- instagram-server/app/Http/Controllers/AuthController.php
+++ instagram-server/app/Http/Controllers/AuthController.php
@@ -9,17 +9,18 @@ use App\Models\User;
 class AuthController extends Controller
 {
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->middleware('auth:api', ['except' => ['login','register']]);
     }
 
-    public function login(Request $request)
-    {
+    public function login(Request $request) {
+        $user = Auth::user();
+
         $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
         ]);
+
         $credentials = $request->only('email', 'password');
 
         $token = Auth::attempt($credentials);
@@ -27,48 +28,46 @@ class AuthController extends Controller
             return response()->json([
                 'status' => 'error',
                 'message' => 'Unauthorized',
+                'token' => $token,
             ], 401);
         }
 
         $user = Auth::user();
+        $user -> token = $token;
+
         return response()->json([
-                'status' => 'success',
-                'user' => $user,
-                'authorization' => [
-                    'token' => $token,
-                    'type' => 'bearer',
-                ]
-            ]);
+            'status' => 'Success',
+            'data' => $user
+            ]
+        );
 
     }
 
-    public function register(Request $request){
+    public function register(Request $request) {
         $request->validate([
             'name' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
         $token = Auth::login($user);
+        $user->token = $token;
+
         return response()->json([
-            'status' => 'success',
-            'message' => 'User created successfully',
-            'user' => $user,
-            'authorization' => [
-                'token' => $token,
-                'type' => 'bearer',
-            ]
+            'status' => 'Success',
+            'data' => $user
         ]);
     }
 
-    public function logout()
-    {
+    public function logout() {
         Auth::logout();
         return response()->json([
             'status' => 'success',
@@ -76,15 +75,13 @@ class AuthController extends Controller
         ]);
     }
 
-    public function refresh()
-    {
+    public function refresh() {
+        $user = Auth::user();
+        $user->token = Auth::refresh();
+
         return response()->json([
-            'status' => 'success',
-            'user' => Auth::user(),
-            'authorisation' => [
-                'token' => Auth::refresh(),
-                'type' => 'bearer',
-            ]
+            'status'=>'success',
+            'data'=>$user
         ]);
     }
 
--- instagram-server/routes/api.php
+++ instagram-server/routes/api.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Auth;
 
-Route::group(["middleware"=>"auth:api"],function(){
-    $user = Auth::user();
+//Route::group(["middleware"=>"auth:api"],function(){
+    
 
     Route::post('register',[AuthController::class,'register']);
     Route::post('login',[AuthController::class,'login']);
-});
+//});
